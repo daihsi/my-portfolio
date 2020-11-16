01d836f6dfe61d4b2396947afb257c975c2d0d14
daihsi <isitomo.mm@gmail.com>
2020-11-16 13:54:03 +0900

    Add: フッター追加

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    Footer
   },
 
   data: () => ({
