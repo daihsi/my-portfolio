5bd7af0698dc8cb0fa1e5f3a526c099effdfa372
daihsi <isitomo.mm@gmail.com>
2020-11-22 07:43:00 +0900

    Add: firebase設定ファイル追加

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,51 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+//gmailの送信メールサーバーアドレス
+const gmailEmail = functions.config().gmail.email;
+//gmailの送信メールサーバーパスワード
+const gmailPassword = functions.config().gmail.password;
+//送信先メールアドレス
+const adminEmail = functions.config().admin.email;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//gmailメールサーバーに関する設定
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    password: gmailPassword
+  }
+});
+
+//メールテンプレート
+const adminContents = data => {
+  return `以下内容でホームページよりお問い合わせを受けました。
+
+お名前：
+${data.name}
+
+メールアドレス：
+${data.email}
+
+内容：
+${data.contents}
+`;
+}
+
+exports.sendMail = functions.https.onCall(async (data, context) => {
+  //メール設定
+  let adminMail = {
+    from: gmailEmail,
+    to: adminEmail,
+    subject: "ホームページお問い合わせ",
+    text: adminContents(data)
+  }
+
+  //管理者へのメール送信
+  try {
+    await mailTransport.sendMail(adminMail);
+  } catch(e) {
+    console.error(`send failed.${e}`);
+    throw new functions.https.HttpsError("internal", "send failed");
+  }
+});
+
